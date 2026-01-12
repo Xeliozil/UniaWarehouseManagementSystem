6671d7d829ad940d6dc70af291562ca550476efd
Xeliozil <lukasz02k@gmail.com>
2026-01-12 18:36:45 +0100

    cleanup?

--- UniaWarehouseManagementSystem/obj/Debug/net6.0-windows/UniaWarehouseManagementSystem.AssemblyInfo.cs
+++ UniaWarehouseManagementSystem/obj/Debug/net6.0-windows/UniaWarehouseManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniaWarehouseManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a706c772f3d5805f33725c9c4a8518bf92428f09")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniaWarehouseManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniaWarehouseManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
